4a1fee1d8232a90be89d7b15913dfb69b738ad04
Babita3 <babita.sima1996@gmail.com>
2022-10-16 09:34:24 +0530

    2nd

--- BridgeLabz/src/Day1_problems/test.java
+++ BridgeLabz/src/Day1_problems/test.java
@@ -6,5 +6,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hi there!");
         System.out.println("i am sima.");
+        System.out.println("i am sima.");
     }
 }
