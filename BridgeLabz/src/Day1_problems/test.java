709ae4ca6721c7c50883a056f1d589e3d18730ab
Babita3 <babita.sima1996@gmail.com>
2022-10-16 08:53:20 +0530

    1st

---
+++ BridgeLabz/src/Day1_problems/test.java
@@ -0,0 +1,10 @@
+package Day1_problems;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hi there!");
+        System.out.println("i am sima.");
+    }
+}
